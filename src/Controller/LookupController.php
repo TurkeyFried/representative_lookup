92fd6dd49f678002ec9574f4c1b55b6bca2bc78a
Luis Pabon <lpabon@thatsus.com>
2020-07-03 15:01:28 -0400

    implementing API calls and the some other contents

--- src/Controller/LookupController.php
+++ src/Controller/LookupController.php
@@ -1,13 +1,79 @@
 <?php
 
-namespace Drupal\canadian_representatives\Controller;
+namespace Drupal\representative_lookup\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\http_client_manager\HttpClientInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines LookupController class.
  */
 class LookupController extends ControllerBase {
+
+  /**
+   * @var \Drupal\http_client_manager\HttpClientInterface
+   */
+  protected $httpClient;
+
+  /**
+   * @param \Drupal\http_client_manager\HttpClientInterface $http_client
+   */
+  public function __construct(HttpClientManagerFactoryInterface $http_client) {
+    $this->httpClient = $http_client;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('representative_lookup.http_client.contents')
+    );
+  }
+
+  /**
+   * Representatives route callback.
+   *
+   * @param string $postal
+   *   The postal code to search in.
+   * @param int $limit
+   *   The total number of representatives we want to fetch.
+   * @param string $sort
+   *   The sorting order.
+   *
+   * @return array
+   *   A render array used to show the Representative list.
+   */
+  public function representatives($postal, $limit, $sort) {
+    $reps = $this->httpClient->getRepresentatives([
+      'postal' => strtoupper($postal),
+      'limit' => (int) $limit,
+      'sort' => $sort,
+    ]);
+
+
+    $build = [
+      '#theme' => 'representative_lookup_representatives_list',
+      '#representatives' => [],
+    ];
+
+    // if ($request->getStatusCode() != 200) {
+    //   return $build;
+    // }
+
+    // $reps = $request->getBody()->getContents();
+
+    foreach ($reps as $rep) {
+      $build['#representatives'][] = [
+        'id' => $rep['id'],
+        'title' => $rep['title'],
+        'text' => $rep['text'],
+      ];
+    }
+
+    return $build;
+  }
 
   /**
    * Display the markup.
@@ -16,9 +82,13 @@ class LookupController extends ControllerBase {
    *   Return markup array.
    */
   public function content() {
+    //@todo page should load form and empty table
+    // then the API call response will trigger the response
+    // how do we use JS here?
     return [
       '#type' => 'markup',
       '#markup' => $this->t('Hello, World!'),
     ];
   }
+
 }
---
+++ src/Form/PostalLookupBlockForm.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\representative_lookup\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Lorem Ipsum block form
+ */
+class PostalLookupBlockForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'postal_lookup_block_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // What's the Postal Code?
+    $form['postal'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Postal Code'),
+      '#default_value' => '',
+      // '#description' => $this->t('Check Representatives for this area'),
+    ];
+
+    // Submit.
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Lookup'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+    // @todo add validation for:
+    // URLs must include the postal code in uppercase letters with no spaces.
+    $postal = $form_state->getValue('postal');
+
+    // $phrases = $form_state->getValue('phrases');
+    // if (!is_numeric($phrases)) {
+    //   $form_state->setErrorByName('phrases', $this->t('Please use a number.'));
+    // }
+
+    // if (floor($phrases) != $phrases) {
+    //   $form_state->setErrorByName('phrases', $this->t('No decimals, please.'));
+    // }
+
+    // if ($phrases < 1) {
+    //   $form_state->setErrorByName('phrases', $this->t('Please use a number greater than zero.'));
+    // }
+  }
+
+  // /**
+  //  * {@inheritdoc}
+  //  */
+  // public function submitForm(array &$form, FormStateInterface $form_state) {
+  //   $form_state->setRedirect('loremipsum.generate', [
+  //     'paragraphs' => $form_state->getValue('paragraphs'),
+  //     'phrases' => $form_state->getValue('phrases'),
+  //   ]);
+  // }
+
+}
